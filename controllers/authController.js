1a19a6f07e1aaeedb19d1d7e3fe33fc50b529ea2
Nisa <n.nurb@hotmail.com>
2022-06-21 15:36:11 +0300

    login page and login processes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
@@ -7,6 +8,29 @@ exports.createUser = async (req, res) => {
     res.status(201).json({
       status: 'success',
       user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //In the new version of mongo, using await with callback gives an error.
+    User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send('YOU ARE LOGGED IN');
+          }
+        });
+      }
     });
   } catch (error) {
     res.status(400).json({
