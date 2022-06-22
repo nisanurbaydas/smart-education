fc000b91704a0e103eccd23235f6b505c0792afb
Nisa <n.nurb@hotmail.com>
2022-06-22 23:30:16 +0300

    refactoring for dashboard, login and register pages, auth and redirect middlewares

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const authMiddleware = require('../middlewares/auth');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
@@ -7,6 +8,6 @@ const router = express.Router();
 router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDashboardPage);
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 
 module.exports = router;
