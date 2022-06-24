307e2e17b7ca562dbb2f7fe325bbc6fcef85441d
Nisa <n.nurb@hotmail.com>
2022-06-24 12:18:15 +0300

    searching

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -24,13 +24,29 @@ exports.getAllCourses = async (req, res) => {
     const categorySlug = req.query.categories;
     const category = await Category.findOne({ slug: categorySlug });
 
+    const query = req.query.search;
+
     let filter = {};
 
     if (categorySlug) {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    if (query) {
+      filter = { name: query };
+    }
+
+    if(!query && !categorySlug) {
+      filter.name = "",
+      filter.category = null
+    }
+
+    const courses = await Course.find({
+      $or:[
+        {name: { $regex: '.*' + filter.name + '.*', $options: 'i'}},
+        {category: filter.category}
+      ]
+    }).sort('-createdAt').populate('user');
 
     const categories = await Category.find();
 
@@ -53,10 +69,12 @@ exports.getCourse = async (req, res) => {
     const course = await Course.findOne({ slug: req.params.slug }).populate(
       'user'
     );
+    const categories = await Category.find();
     res.status(200).render('course', {
       course,
       page_name: 'courses',
       user,
+      categories
     });
   } catch (error) {
     res.status(400).json({
