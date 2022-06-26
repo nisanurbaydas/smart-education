096afc86b15edd622d9c7278949663eb27413069
Nisa <n.nurb@hotmail.com>
2022-06-26 19:20:47 +0300

    validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/User');
 const Category = require('../models/Category');
@@ -10,10 +11,15 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    //console.log(errors);
+    //console.log(errors.array()[0].msg);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash('error', `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect('/register');
   }
 };
 
@@ -25,10 +31,18 @@ exports.loginUser = (req, res) => {
     User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
             // USER SESSION
             req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
+          } else {
+            req.flash('error', 'Your password is not correct!');
+            res.status(400).redirect('/login');
+          }
         });
+      } else {
+        req.flash('error', 'There is no such user.');
+        res.status(400).redirect('/login');
       }
     });
   } catch (error) {
@@ -46,13 +60,15 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID }).populate('courses');
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    'courses'
+  );
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
     categories,
-    courses
+    courses,
   });
 };
